47953741272d3942e43edbe920f68830ab77bc83
Mavis Ou <mmmavisss@gmail.com>
2015-07-30 19:14:18 -0700

    finish building the new Webmaker email template

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # BSD email template
 
 experimental setup for building BSD email template
+
+# Prerequisites
+
+- install
+- `rvm pkg install libyaml`
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var path = require('path');
+var fs = require('fs');
 var less = require('gulp-less');
-var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var fileinclude = require('gulp-file-include');
 
+var premailer = require('gulp-premailer');
+
 gutil.log(gutil.colors.blue(" __dirname:", __dirname));
 
-gulp.task('fileinclude', ['less'], function() {
+gulp.task('compile-and-copy-files', ['less'], function() {
   gulp.src('./src/**/index-no-premailer.html')
       .pipe(fileinclude({
         prefix: '@@',
@@ -17,19 +19,35 @@ gulp.task('fileinclude', ['less'], function() {
 
   gulp.src('./src/**/index-premailered.html')
       .pipe(gulp.dest('./compiled'));
+
+  gulp.src('./src/**/img/**')
+      .pipe(gulp.dest('./compiled'));
+});
+
+gulp.task('premailer', function() {
+  fs.exists('./compiled', function(exists) {
+    if (exists) {
+      gulp.src('./compiled/**/index-no-premailer.html')
+        .pipe(premailer())
+        .pipe(gulp.dest('./premailer-formatted'));
+    } else {
+      gutil.log(gutil.colors.red("[Error] Make sure your source files are compiled first."));
+    }
+  })
 });
 
 gulp.task('less', function () {
   gutil.log(gutil.colors.blue("compiling less..."));
   return gulp.src('./src/**/style.less')
-    .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(gulp.dest('./src'));
 });
 
 gulp.task('watch', function() {
   gutil.log(gutil.colors.blue("watching..."));
-  gulp.watch(['./src/**/*.html', './src/**/style.less'], ['fileinclude']);
+  gulp.watch(['./src/**/*.html', './src/**/style.less'], ['compile-and-copy-files']);
 });
 
-gulp.task("start", [ "watch" ]);
+gulp.task('build', ['premailer'], function() {
+  gutil.log(gutil.colors.blue("running premailer..."));
+});
